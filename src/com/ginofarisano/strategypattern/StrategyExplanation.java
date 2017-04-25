05bb67fcd53e7f550ec5084f3579847f1385a5e9
ginofarisano <ginofarisano@gmail.com>
2017-04-25 20:47:31 +0200

    Add somo code refs

--- src/com/ginofarisano/strategypattern/StrategyExplanation.java
+++ src/com/ginofarisano/strategypattern/StrategyExplanation.java
@@ -1,6 +1,7 @@
 /*
 You use this pattern if you need to dynamically change an algorithm used by an object at run time.
-For more details: https://en.wikipedia.org/wiki/Strategy_pattern
+For more details: https://it.wikipedia.org/wiki/Strategy_pattern 
+https://en.wikipedia.org/wiki/Strategy_pattern
 
 I implement this pattern with java generics so the SortStrategy is decoupled by a specific implementation.
 You can use any class that implements comparable.
